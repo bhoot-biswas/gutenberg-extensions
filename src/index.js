43a9412a0bf4b4944cfad399aedd13cb50008f89
Mithun Biswas <bhoot.biswas@gmail.com>
2020-11-27 14:03:06 +0600

    Add addDisplayControlAttribute()

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('hh');
+const enableDisplayControlOnBlocks = ["core/paragraph", "core/image"];
+
+/**
+ * [addDisplayControlAttribute description]
+ * @param {[type]} settings [description]
+ * @param {[type]} name     [description]
+ */
+function addDisplayControlAttribute(settings, name) {
+	if (!enableDisplayControlOnBlocks.includes(name)) {
+		return settings;
+	}
+
+	return {
+		...settings,
+		hideOnMobile: {
+			type: "boolean",
+			default: false
+		}
+	};
+}
+
+addFilter(
+	"blocks.registerBlockType",
+	"gutenberg-extensions/attribute/display",
+	addDisplayControlAttribute
+);
