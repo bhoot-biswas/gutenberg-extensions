34e0a4cc22f6c63b60207420da73bda76426f4a5
Mithun Biswas <bhoot.biswas@gmail.com>
2020-11-27 12:45:41 +0600

    Add initial files

---
+++ gutenberg-extensions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Gutenberg Extensions
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     gutenberg-extensions
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Gutenberg_Extensions
+ */
+
+// Your code starts here.
